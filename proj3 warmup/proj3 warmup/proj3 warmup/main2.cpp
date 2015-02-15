5a92b01baeea195f61f1fc65559c01a4292086e3
ethrze <ethrze@gmail.com>
2015-02-15 00:15:47 -0800

    proj3 warmup work and new spac

--- proj3 warmup/proj3 warmup/proj3 warmup/main2.cpp
+++ proj3 warmup/proj3 warmup/proj3 warmup/main2.cpp
@@ -1,41 +1,35 @@
-////
-////  main.cpp
-////  proj3 warmup
-////
-//
-//#include <list>
 //#include <vector>
 //#include <algorithm>
 //#include <iostream>
 //#include <cassert>
 //using namespace std;
 //
-//// Remove the odd integers from li.
+//// Remove the odd integers from v.
 //// It is acceptable if the order of the remaining even integers is not
-//// the same as in the original list.
-//void removeOdds(list<int>& li)
+//// the same as in the original vector.
+//void removeOdds(vector<int>& v)
 //{
-//    list<int> editLi;
-//    for (list<int>::iterator p = li.begin(); p != li.end(); p++)
+//    vector<int> editV;
+//    for (vector<int>::iterator p = v.begin(); p != v.end(); p++)
 //    {
 //        if (*p % 2 == 0)
-//            editLi.push_back(*p);
+//            editV.push_back(*p);
 //    }
-//    li = editLi;
+//    v = editV;
+//    
 //}
 //
 //void test()
 //{
 //    int a[8] = { 2, 8, 5, 6, 7, 3, 4, 1 };
-//    list<int> x(a, a+8);  // construct x from the array
+//    vector<int> x(a, a+8);  // construct x from the array
 //    assert(x.size() == 8 && x.front() == 2 && x.back() == 1);
 //    removeOdds(x);
 //    assert(x.size() == 4);
-//    vector<int> v(x.begin(), x.end());  // construct v from x
-//    sort(v.begin(), v.end());
+//    sort(x.begin(), x.end());
 //    int expect[4] = { 2, 4, 6, 8 };
 //    for (int k = 0; k < 4; k++)
-//        assert(v[k] == expect[k]);
+//        assert(x[k] == expect[k]);
 //}
 //
 //int main()
