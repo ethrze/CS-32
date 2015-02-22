f10c3ff45399d524e92b92f0857a868fbf29e33f
ethrze <ethrze@gmail.com>
2015-02-21 16:03:12 -0800

    started building out actor header

--- proj3/BoulderBlast/BoulderBlast/Actor.h
+++ proj3/BoulderBlast/BoulderBlast/Actor.h
@@ -10,19 +10,46 @@
 
 class Actor : public GraphObject {
 public:
-    Actor()
-    : GraphObject(IID_WALL, 0, 0, none)
-    { }
+    Actor(const int IID, int sx, int sy)
+    : GraphObject(IID, sx, sy, none) // the closest you can get to a generic GraphObject
+    { } // may need to put in checks on sx sy
+    
     virtual void doSomething() {
         // the actor does something
     }
     virtual ~Actor() {}
+    
 private:
     
     
 };
+// this is technically all that's specifically required for P1
 
-
+class Player : public Actor {
+public:
+    Player(int sx, int sy)
+    : Actor(IID_PLAYER, sx, sy), m_dead(0)
+    { }
+    
+    virtual void doSomething() {
+        
+    }
+    
+    
+    virtual bool amIDead()
+    {
+        return m_dead;
+    }
+    virtual void kill()
+    {
+        m_dead = true;
+    }
+    virtual ~Player() {}
+    
+private:
+    bool m_dead;
+    
+};
 
 
 #endif // ACTOR_H_
