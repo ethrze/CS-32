2c120a50d5663e2179aef421ea69ec86414a87a4
ethrze <ethrze@gmail.com>
2015-03-09 22:52:23 -0700

    trying to hashtable; trying real hard

--- proj4/project4/project4/HashTable.h
+++ proj4/project4/project4/HashTable.h
@@ -34,23 +34,41 @@ private:
     
     class Node {
     public:
-        Node(int value)
-        : m_key(string()), m_value(value), m_next(nullptr)
-        {}
-        void setVal(ValueType val) { m_value = val; }
-        void setKey(KeyType key) { m_key = key; }
-        void setNext(Node* nex) { m_next = nex; }
-        ValueType getVal() { return m_value; }
-        Node* getNext() { return m_next; }
-        void age() { m_age++; }
+        Node(KeyType key, ValueType val, bool permanent)
+        : m_key(key), m_value(val), m_age(0), m_next(nullptr), m_permanent(permanent)
+        {
+            if (permanent)
+                m_age = -1;
+        }
     private:
-        KeyType m_key;
         ValueType m_value;
+        KeyType m_key;
+        bool m_permanent;
         Node* m_next;
         int m_age;
     };
     
-    ValueType* m_bucketList;
+    class Bucket {
+    public:
+        Bucket(int goodHash)
+        : m_hash(goodHash)
+        { }
+        void initContent(Node* first)
+        { m_contents = first; }
+        int getHash() { return m_hash; }
+    private:
+        int m_hash;
+        int m_nWithin;
+        Node* m_contents;
+    };
+    
+    void growOld();
+    
+    
+    Bucket* m_buckList;
+//    Node* m_bucketRoot;
+    int m_nBuckets;
+    int m_curBuckets;
     int m_capacity;
     int m_fill;
 };
@@ -59,7 +77,8 @@ template<typename KeyType, typename ValueType>
 HashTable<KeyType, ValueType>::HashTable(int numBuckets, int capacity)
 {
     m_capacity = capacity;
-    m_bucketList = new ValueType[numBuckets];
+    m_buckList = new Bucket[numBuckets];
+    m_nBuckets = numBuckets;
 }
 template<typename KeyType, typename ValueType>
 HashTable<KeyType, ValueType>::~HashTable()
@@ -76,8 +95,62 @@ bool HashTable<KeyType, ValueType>::isFull() const
 template<typename KeyType, typename ValueType>
 bool HashTable<KeyType, ValueType>::set(const KeyType& key, const ValueType& value, bool permanent)
 {
+    bool allIn = false;
+    if (!permanent)
+        growOld();
     // default for permanent is false
+    for (int i = 0; i < this->m_nBuckets; i++)
+    {
+        if (computeHash(key) == m_buckList[i].m_hash) // if it already has a bucket
+        {
+            if (!isFull())
+            {
+                auto m = m_buckList[i].m_contents;
+                bool update = false;
+                while (m->m_next != nullptr)
+                {
+                    m = m->m_next;
+                    if (m->m_key == key)
+                        update = true;
+                }
+                if (!update)
+                {
+                    Node* nodey = new Node(key, value, permanent);
+                    m->m_next = nodey;
+                    m_buckList[i]->m_nWithin++; // bucket size tracker
+                    m_fill++; // capacity counter
+                    allIn = true;
+                }
+                if (update)
+                {
+                    auto t = m_buckList[i].m_contents;
+                    while(t->m_key != key)
+                        t = t->m_next;
+                    t->value = value;
+                    // successful update
+                    allIn = true;
+                }
+            }
+        }
+    }
+    if (!isFull() && allIn == false) // if it just doesn't have a bucket yet.
+    {
+        if (m_curBuckets != m_nBuckets)
+        {
+            m_buckList[m_curBuckets] = new Bucket(computeHash(key)); // we made a bucket!
+            Node* shorthand = m_buckList[m_curBuckets]->m_contents;
+            Node* nodey = new Node (key, value, permanent);
+            shorthand = nodey;
+            m_curBuckets++; // bucket size tracker
+            m_fill++; // capacity tracker
+            allIn = true;
+        }
+    }
+
+    if (allIn == true)
+        return true;
     return false;
+    
 }
 template<typename KeyType, typename ValueType>
 bool HashTable<KeyType, ValueType>::get(const KeyType& key, ValueType& value) const
@@ -93,6 +166,22 @@ template<typename KeyType, typename ValueType>
 bool HashTable<KeyType, ValueType>::discard(KeyType& key, ValueType& value)
 {
     return false;
+}
+template<typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::growOld()
+{
+    // HOUSEKEEPING
+    for (int i = 0; i < this->m_curBuckets; i++) // age all the buckets
+    {
+        if (m_buckList->m_contents != nullptr)
+        {
+            for (Node* q = m_buckList->m_contents; q != nullptr; q = q->m_next)
+            {
+                if (!(q->m_permanent))
+                    q->age();
+            }
+        }
+    }
 }
 
 
