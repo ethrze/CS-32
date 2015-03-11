c57c773841d4659751a6241f26cb7fbb66e06aca
ethrze <ethrze@gmail.com>
2015-03-10 23:12:15 -0700

    v2 setup complete

--- proj4/skeleton/Compressor.cpp
+++ proj4/skeleton/Compressor.cpp
@@ -1,7 +1,21 @@
+//     _
+//    u            .--~*teu.
+//   88Nu.   u.   dF     988Nx
+//  '88888.o888c d888b   `8888>
+//   ^8888  8888 ?8888>  98888F
+//    8888  8888  "**"  x88888~
+//    8888  8888       d8888*`
+//    8888  8888     z8**"`   :
+//   .8888b.888P   :?.....  ..F
+//    ^Y8888*""   <""888888888~
+//      `Y"       8:  "888888*
+//                ""    "**"`
+
 #include "provided.h"
 #include <string>
 #include <vector>
 using namespace std;
+
 
 void Compressor::compress(const string& s, vector<unsigned short>& numbers)
 {
