79b0883e0e38347ff7c94c7a23080f8d5f835bb5
ethrze <ethrze@gmail.com>
2015-03-12 13:11:09 -0700

    trying to write compressor

--- proj4/v2/vs2/vs2/Compressor.cpp
+++ proj4/v2/vs2/vs2/Compressor.cpp
@@ -1,11 +1,29 @@
 #include "provided.h"
+#include "substituteHashTable.h"
 #include <string>
 #include <vector>
 using namespace std;
 
+
 void Compressor::compress(const string& s, vector<unsigned short>& numbers)
 {
 	  // This compiles, but may not be correct
+    int cap;
+    int len = (int)(s.length());
+    if (16384 > (len/2 + 512))
+        cap = (len/2 + 512);
+    else
+        cap = 16384;
+    
+    // cap / nBuckets must be <= 0.5
+    int nBuckets = cap * 2;
+        
+    HashTable<string, unsigned short> H(nBuckets, cap);
+    for (int i = 0; i < 256; i++)
+    {
+        
+    }
+    
 }
 
 bool Compressor::decompress(const vector<unsigned short>& numbers, string& s)
