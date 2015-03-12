91269ce9d012bf1b832a0b3822666141700211e8
ethrze <ethrze@gmail.com>
2015-03-12 16:47:12 -0700

    compress function works

--- proj4/v2/vs2/vs2/Compressor.cpp
+++ proj4/v2/vs2/vs2/Compressor.cpp
@@ -2,12 +2,13 @@
 #include "substituteHashTable.h"
 #include <string>
 #include <vector>
+#include <iostream>
 using namespace std;
 
 
 void Compressor::compress(const string& s, vector<unsigned short>& numbers)
 {
-	  // This compiles, but may not be correct
+    // calculate capacity
     int cap;
     int len = (int)(s.length());
     if (16384 > (len/2 + 512))
@@ -17,30 +18,43 @@ void Compressor::compress(const string& s, vector<unsigned short>& numbers)
     
     // cap / nBuckets must be <= 0.5
     int nBuckets = cap * 2;
-        
+    // create Hashtable
     HashTable<string, unsigned short> H(nBuckets, cap);
+    // 2. populate hash table
     for (unsigned short j = 0; j < 256; j++)
     {
 //        char charj = static_cast<char>(j);
-        string charj;
+        string charj = string();
         charj += static_cast<char>(j);
         H.set(charj, j, true);
     }
     
+    // 3. set nextFreeID
     int nextFreeID = 256;
-    string runSoFar = "";
+    // 4. create empty string runSoFar
+    string runSoFar = string();
+    // 5. create empty vecotr V to contain compressed result
     vector<unsigned short> V;
+    // 6. for each character 'c' in input string,
     for (int i = 0; i < s.length(); i++) // step 6
     {
+        // weird casting
         char m = s[i];
-        string c;
+        string c = "";
         c += m; // monkey business
+        // a. create string named expandedRun equal to runSoFar + c
         string expandedRun = runSoFar + c;
+        // b. if expandedRun is already contained in H
         unsigned short x;
         if (H.get(expandedRun, x) == true) // if expanded run is in the HashTable
         {
+            // i. set runSoFar equal to expandedRun, inc current run length by one char
             runSoFar = expandedRun;
-        } else {
+            // ii. go to next iteration of step 6 above.
+            continue;
+        }
+        else
+        {
             H.get(runSoFar, x);
         }
         V.push_back(x);
@@ -62,7 +76,7 @@ void Compressor::compress(const string& s, vector<unsigned short>& numbers)
             H.set(expandedRun, disc);
         }
     }
-    // step 6 complete
+    // step 7
     if (runSoFar != "")
     {
         unsigned short x;
